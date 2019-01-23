a7649c5dea53377755ac107841aa582755d75b74
Sandy Ynag <yang21@uw.edu>
2019-01-22 20:21:38 -0800

    exercise

--- week-3/lab3-exercise.R
+++ week-3/lab3-exercise.R
@@ -2,14 +2,22 @@ data(iris)
 View(iris)
 
 #Get all rows of Species ‘versicolor’ in a new data frame. Call this data frame: ‘iris.vers’
+iris.vers <- iris[iris$Species == "versicolor", ]
 
 #Get a vector called ‘sepal.dif’ with the difference between ‘Sepal.Length’ and ‘Sepal.Width’ of ‘versicolor’ plants.
+sepal.dif <- abs(iris.vers$Sepal.Length - iris.vers$Sepal.Width)
 
 #Add ‘iris.vers’ with the new column ‘sepal.dif’.
+iris.vers$sepal.dif <- sepal.dif
 
 #filter for all data of Species ‘virginica’ with a ‘Sepal.Width’ of greater than 3.5.
+virginica <- iris[iris$Species == "virginica", ]
+virginica[virginica$Sepal.Width > 3.5, ]
 
 #get a data frame of  all data of Species ‘setosa’ with a ‘Sepal.Width’ of greater than 3.5, but without the last column Species in the data frame?
+all_setosa <- iris[iris$Species == "setosa", ]
+setosa_wid <- all_setosa[virginica$Sepal.Width > 3.5, ]
+setosa_wid[1:nrow(setosa_wid)-1, ]
 
 #Write a function that takes in a species name as an argument and returns the average Sepal Width of that species.
 
